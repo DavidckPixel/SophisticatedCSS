1899e73474e62634d0a597f9206880e5c7655b26
Jasper Weyne <jasperweyne@gmail.com>
2021-03-10 16:33:55 +0100

    Functional item detection and rendering

--- src/script/index.ts
+++ src/script/index.ts
@@ -2,12 +2,17 @@
 /// <reference path="./Selector/SelectorRenderer.ts" />
 
 // Get the menu node
-// TODO: retrieve menu node
-let menuNode = document.body;
+function buildSelector() {
+    // TODO: retrieve menu node
+    let menuNode = document.currentScript?.parentElement;
+    if (!menuNode) {
+        throw new Error("buildSelector() can't be run from an event handler, timeout or module");
+    }
 
-// Initialize the component selector menu
-let nodes = new SelectorBuilder();
-let data = nodes.build(document.body);
-console.log(data);
-let renderer = new SelectorRenderer(data);
-renderer.render(menuNode);
+    // Initialize the component selector menu
+    let nodes = new SelectorBuilder();
+    let data = nodes.build(document.body);
+    console.log(data);
+    let renderer = new SelectorRenderer(data);
+    renderer.render(menuNode);
+}
