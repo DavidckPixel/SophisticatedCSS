8f32066b75a30c880e6432823bdc26982b8cd473
David <davidc.koynans@outlook.com>
2021-04-13 14:31:45 +0200

    Added beginnin for login

--- src/server/init/router.ts
+++ src/server/init/router.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { Database } from "../database";
 import registerApi from "../views/api";
 import registerSecurity from "../views/security";
+import bodyparser from "body-parser";
 
 export default function router(db: Database): Express {
     const app = express();
@@ -11,6 +12,7 @@ export default function router(db: Database): Express {
     app.get( "/dist/index.js", ( req, res ) => {
         res.sendFile( "dist/index.js", { root: '.' } );
     } );
+    app.use(bodyparser());
 
     // Dynamic content
     registerApi(app, db);
--- src/server/views/security.ts
+++ src/server/views/security.ts
@@ -1,6 +1,9 @@
 import asyncHandler from "express-async-handler";
 import { Express } from "express";
 import { Database } from "../database";
+import * as argon2 from "argon2";
+import { User } from "../entities"
+
 
 export default function register(app: Express, db: Database) {
     /**
@@ -14,6 +17,25 @@ export default function register(app: Express, db: Database) {
      * Login form, user posted their login to the server
      */
     app.post('/login', asyncHandler(async (req, res) => {
-        // todo: check whether user logged in correctly
+        //res.json(req.body);
+
+        let data = db.repository(User);
+        let user = await data.find(req.body.username);
+
+        if(!user){
+            res.json({responde:"unkown user"})
+            return;
+        }
+
+        try{
+            if (await argon2.verify(user.getPassword(),req.body.password.hashCode())){
+                res.json({responde:"Login succesfull"})
+            }
+            else{
+                res.json({responde:"wrong password"})
+            }
+        }catch(err){
+
+        }
     }));
 }
