6d38664745091c424a7e70117c134637a6510538
Jasper Weyne <jasperweyne@gmail.com>
2021-04-14 10:37:45 +0200

    Simple ViewComponent

---
+++ src/client/component/ViewComponent.ts
@@ -0,0 +1,75 @@
+abstract class ViewComponent {
+    private parent: ViewComponent | undefined;
+
+    private location: HTMLElement | undefined;
+
+    private state: any;
+    
+    public abstract render(state: any): HTMLElement;
+
+    /**
+     * Mount the object to the DOM
+     * 
+     * @param dest The HTML element to mount it to
+     */
+    public mountTo(dest: HTMLElement|ViewComponent) {
+        if (dest instanceof ViewComponent) {
+            this.parent = dest;
+        } else {
+            this.location = dest.appendChild(this.render(this.state));
+        }
+    }
+    
+    /**
+     * Unmount the mounted object
+     */
+    public unmount() {
+        this.location?.remove();
+        delete this.location;
+        delete this.parent;
+    }
+
+    /**
+     * Update the state of the component and re-render it
+     * 
+     * @param state 
+     */
+    protected setState(state: any): void
+    {
+        // Update the state
+        this.state = state;
+
+        // If parented, update
+        this.parent?.setState(this.parent.state);
+
+        // If mounted, update
+        this.location?.replaceWith(this.render(this.state));
+    }
+
+    /**
+     * Create a HTML element node with properties and children
+     * 
+     * @param type The HTML element type.
+     * @param props An object containing the properties for the element.
+     * @param children The child nodes for the element.
+     */
+    protected create(type: string, props?: any, ...children: (Node|string)[]): HTMLElement | HTMLInputElement {
+        // Create element
+        let el = document.createElement(type);
+
+        // Assign properties
+        Object.assign(el, props);
+
+        // Add children
+        for (const child of children) {
+            if (typeof child === 'string') {
+                el.appendChild(document.createTextNode(child));
+            } else if (child instanceof Node) {
+                el.appendChild(child);
+            }
+        }
+
+        // Finally return new element
+        return el;
+    }
+}
