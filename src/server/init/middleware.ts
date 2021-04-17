6c24b83cd11fed9bc7d622b5e4de66df600a686d
Jasper Weyne <jasperweyne@gmail.com>
2021-04-17 14:29:43 +0200

    Write logs to access.log

--- src/server/init/middleware.ts
+++ src/server/init/middleware.ts
@@ -5,13 +5,17 @@ import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import morgan from "morgan";
 import helmet from "helmet";
+import fs from "fs";
+import path from "path";
 import { Database } from "../database";
 import { verifyCredentials, serialize, deserialize } from "../security"; 
 
 export default function middleware(app: Express, db: Database): Express {
+    const accessLogStream = fs.createWriteStream(path.join(__dirname, '/../../../access.log'), { flags: 'a' })
+
     app.set("view engine", "ejs");
     app.use(helmet());
-    app.use(morgan('combined'));
+    app.use(morgan('combined', { stream: accessLogStream }));
     app.use(express.json())
     app.use(express.urlencoded());
     app.use(session({ secret: 'ThisIsNotSoSecret', resave: false, saveUninitialized: false, cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } }));
